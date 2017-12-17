6efe38780a325a64c8f5d5d1498fd71ad16ce30d
Cayde-14 <33264120+Cayde-14@users.noreply.github.com>
2017-12-17 13:07:08 -0500

    Udpate3

--- catscript.py
+++ catscript.py
@@ -1,17 +1,19 @@
 # Scan system for removeable drives & prompt user to select the drive they wish to fuck
-def locate_usb():
 import win32file
-drive_list = []
-drivebits=win32file.GetLogicalDrives()
-for d in range(1,26):
-    mask=1 << d
-    if drivebits & mask:
-        # here if the drive is at least there
-        drname='%c:\\' % chr(ord('A')+d)
-        t=win32file.GetDriveType(drname)
-        if t == win32file.DRIVE_REMOVABLE:
-            drive_list.append(drname)
-return drive_list
+
+def locate_usb():
+	drive_list = []
+	drivebits=win32file.GetLogicalDrives()
+	for d in range(1,26):
+		mask=1 << d
+		if drivebits & mask:
+			# here if the drive is at least there
+			drname='%c:\\' % chr(ord('A')+d)
+			t=win32file.GetDriveType(drname)
+			if t == win32file.DRIVE_REMOVABLE:
+				drive_list.append(drname)
+	return drive_list
+	print (drive_list)
 
 locate_usb()
 
