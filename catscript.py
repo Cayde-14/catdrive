bf5183b9f7f9603517d2be00eef4cccd20586936
Keith <kdesimini@bsu.edu>
2017-12-17 14:42:24 -0500

    Changed code around

--- catscript.py
+++ catscript.py
@@ -1,43 +1,26 @@
 import os
 import shutil
-import ctypes
 
-# Drive types
-DRIVE_UNKNOWN     = 0  # The drive type cannot be determined.
-DRIVE_NO_ROOT_DIR = 1  # The root path is invalid; for example, there is no volume mounted at the specified path.
-DRIVE_REMOVABLE   = 2  # The drive has removable media; for example, a floppy drive, thumb drive, or flash card reader.
-DRIVE_FIXED       = 3  # The drive has fixed media; for example, a hard disk drive or flash drive.
-DRIVE_REMOTE      = 4  # The drive is a remote (network) drive.
-DRIVE_CDROM       = 5  # The drive is a CD-ROM drive.
-DRIVE_RAMDISK     = 6  # The drive is a RAM disk.
-
-# Map drive types to strings
-DRIVE_TYPE_MAP = { DRIVE_UNKNOWN     : 'DRIVE_UNKNOWN',
-                   DRIVE_NO_ROOT_DIR : 'DRIVE_NO_ROOT_DIR',
-                   DRIVE_REMOVABLE   : 'DRIVE_REMOVABLE',
-                   DRIVE_FIXED       : 'DRIVE_FIXED',
-                   DRIVE_REMOTE      : 'DRIVE_REMOTE',
-                   DRIVE_CDROM       : 'DRIVE_CDROM',
-                   DRIVE_RAMDISK     : 'DRIVE_RAMDISK'}
+# Check current working directory.
+path = os.getcwd()
+retval = os.getcwd()
+print ("Current working directory %s" % retval)
 
 
-# Return list of tuples mapping drive letters to drive types
-def get_drive_info():
-    result = []
-    bitmask = ctypes.windll.kernel32.GetLogicalDrives()
-    for i in range(26):
-        bit = 2 ** i
-        if bit & bitmask:
-            drive_letter = '%s:' % chr(65 + i)
-            drive_type = ctypes.windll.kernel32.GetDriveTypeA('%s\\' % drive_letter)
-            result.append((drive_letter, drive_type))
-    return result
 
 
-# Test
-if __name__ == '__main__':
-    drive_info = get_drive_info()
-    for drive_letter, drive_type in drive_info:
-        print ('%s = %s' % (drive_letter, DRIVE_TYPE_MAP[drive_type]))
-    removable_drives = [drive_letter for drive_letter, drive_type in drive_info if drive_type == DRIVE_REMOVABLE]
-    print ('removable_drives = %r' % removable_drives)
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+print (get_size(path))
+
+def copy_functionUSB():
+	size = get_size()
+	while size > 1000:
+		print("hello bitches")
+		
