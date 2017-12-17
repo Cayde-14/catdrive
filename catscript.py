128086b455ff54342a7b1b0ea889efae1b13676e
Cayde-14 <33264120+Cayde-14@users.noreply.github.com>
2017-12-17 11:34:48 -0500

    Possible system scan for removeable drives
    Reference: https://stackoverflow.com/questions/4273252/detect-inserted-usb-on-windows

---
+++ catscript.py
@@ -0,0 +1,14 @@
+# Scan system for removeable drives & prompt user to select the drive they wish to fuck
+def locate_usb():
+import win32file
+drive_list = []
+drivebits=win32file.GetLogicalDrives()
+for d in range(1,26):
+    mask=1 << d
+    if drivebits & mask:
+        # here if the drive is at least there
+        drname='%c:\\' % chr(ord('A')+d)
+        t=win32file.GetDriveType(drname)
+        if t == win32file.DRIVE_REMOVABLE:
+            drive_list.append(drname)
+return drive_list
