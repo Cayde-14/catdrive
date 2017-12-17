014770da2386f8eacdc9bdb4f0aa2d823bdf86cd
Keith <kdesimini@bsu.edu>
2017-12-17 13:15:28 -0500

    Revert "Udpate3"
    This reverts commit 6efe38780a325a64c8f5d5d1498fd71ad16ce30d.

--- catscript.py
+++ catscript.py
@@ -46,21 +46,19 @@ if __name__ == '__main__':
 
 =======
 # Scan system for removeable drives & prompt user to select the drive they wish to fuck
-import win32file
-
 def locate_usb():
-	drive_list = []
-	drivebits=win32file.GetLogicalDrives()
-	for d in range(1,26):
-		mask=1 << d
-		if drivebits & mask:
-			# here if the drive is at least there
-			drname='%c:\\' % chr(ord('A')+d)
-			t=win32file.GetDriveType(drname)
-			if t == win32file.DRIVE_REMOVABLE:
-				drive_list.append(drname)
-	return drive_list
-	print (drive_list)
+import win32file
+drive_list = []
+drivebits=win32file.GetLogicalDrives()
+for d in range(1,26):
+    mask=1 << d
+    if drivebits & mask:
+        # here if the drive is at least there
+        drname='%c:\\' % chr(ord('A')+d)
+        t=win32file.GetDriveType(drname)
+        if t == win32file.DRIVE_REMOVABLE:
+            drive_list.append(drname)
+return drive_list
 
 locate_usb()
 >>>>>>> 6efe38780a325a64c8f5d5d1498fd71ad16ce30d
